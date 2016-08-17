b627d664ef965bca528a53006f7bcc94b2c0db5b
Katherine Tabinowski <ktab@katherines-mbp.fios-router.home>
2016-08-17 15:04:09 -0400

    created alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,21 @@ cardOne = "queen";
 cardTwo = "queen";
 cardThree = "king";
 cardFour = "king";
+
+if (cardTwo === cardFour) {
+
+window.alert("Sorry, try again.");
+
+} else if (cardOne === cardThree) {
+
+	window.alert("Sorry, try again.");
+} 
+else if (cardOne === cardTwo) {
+
+	window.alert("You found a match!");
+}
+
+else if (cardThree === cardFour) {
+
+	window.alert("You found a match!");
+}
