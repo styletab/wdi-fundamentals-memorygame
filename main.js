2bd8743e15b5e92d57bd81eaf3cd203d886a6611
styletab <katherinetab@gmail.com>
2016-08-28 14:55:34 -0400

    Update All Files
    Re-saved memory game files to ensure they're up-to-date from previous session.

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("JS file is connected to HTML! Woo!");
+
+cardOne = "queen";
+cardTwo = "queen";
+cardThree = "king";
+cardFour = "king";
+
+/* if (cardOne === cardTwo) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
+
+*/
+
+var board = document.getElementsById('game-board');
+
+function createBoard() {
+
+for(var i = 0; i < card.length; i++) {
+
+	var cardElement = document.createELement('div');
+
+	cardElement.className = 'card';
+
+	board.appendChild(cardElement);
+}
+
+}
